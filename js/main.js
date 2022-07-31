83f6603ea24414137f5419922f3757ec78223d63
Igor Negin <igornegin@yandex.ru>
2022-08-01 00:06:17 +0300

    create section navigation and header

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
 "use strict";
+
+// Navigation fixed
+const navigation = document.querySelector('.navigation');
+
+window.addEventListener('scroll', function(){
+    if(this.scrollY > 0) {
+        navigation.classList.add('fixed');
+    } else {
+        navigation.classList.remove('fixed');
+    }
+
+    // window scroll - close navigation bar
+    navButton.classList.remove('uil-multiply');
+    navLinks.classList.remove('active');
+});
+
+// Open-close menu-bar
+const navLinks = document.querySelector('.nav__links'),
+      navButton = document.querySelector('#open__menu');
+
+navButton.addEventListener('click', function(){
+    this.classList.toggle('uil-multiply');
+    navLinks.classList.toggle('active');
+});
